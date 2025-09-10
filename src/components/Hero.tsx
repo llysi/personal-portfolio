9cf38cc6ec3765e832031bd8791a8b1e4afb728c
llysi <zhaoalysa2006@gmail.com>
2025-09-10 13:45:21 -0500

    Add hero skeleton

---
+++ src/components/Hero.tsx
@@ -0,0 +1,29 @@
+export default function Hero() {
+    return (
+        <section className="/* TODO: Add container classes */">
+        {/* TODO: Add background gradient */}
+
+        <div className="/* TODO: Add responsive padding and centering */">
+            {/* Name - Large heading */}
+            <h1 className="/* TODO: Responsive typography */">
+            Alysa Zhao
+            </h1>
+
+            {/* Tagline */}
+            <p className="/* TODO: Secondary text styling */">
+                Full-stack developer and digital artist. Learning AI.
+            </p>
+
+            {/* CTA Button */}
+            <button className="/* TODO: Primary button styling */">
+                Contact Me
+            </button>
+
+            {/* Social Links */}
+            <div className="/* TODO: Social links layout */">
+                {/* Email, GitHub, LinkedIn links */}
+            </div>
+            </div>
+        </section>
+    )
+}
