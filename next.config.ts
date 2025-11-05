acb177321f7cbd63f2f5e9411396e66930d39d64
llysi <zhaoalysa2006@gmail.com>
2025-11-04 21:34:12 -0600

    comment out basepath

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/personal-portfolio',
+  //basePath: '/personal-portfolio',
   images: {
     unoptimized: true,
   },
