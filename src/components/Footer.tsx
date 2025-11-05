4fbdf3b364e8bbadf3489502d9e023e3ecdd585c
llysi <zhaoalysa2006@gmail.com>
2025-11-05 00:13:11 -0600

    fix apostrophe

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
         <footer className="w-full mt-auto text-textcolor/60">
             <div className="max-w-5xl mx-auto px-8 py-6 flex flex-col md:flex-row justify-between items-start md:items-center gap-4">
                 <div className="flex items-center gap-2">
-                    <p className="text-sm">Let's create!</p>
+                    <p className="text-sm">Let&apos;s create!</p>
                     {/* Email */}
                     <a
                         href="mailto:alysazhao111@tamu.edu"
